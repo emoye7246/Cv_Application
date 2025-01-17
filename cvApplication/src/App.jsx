30b831d24d96b377b4c59bfc96e91effbeb8aabe
Elijah <elijahmoye96@gmail.com>
2025-01-17 02:02:16 -0500

    Great day
    So as im proceeeding woth the react project I am
    finding new way to break down and place components
    in the right spaces

--- cvApplication/src/App.jsx
+++ cvApplication/src/App.jsx
@@ -1,11 +1,85 @@
-import { Sidebar } from "./jsx/sidebar"
+import '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/css/cvDisplay.css'
+import { useState } from 'react'
 
 export function App(){
+
+    const [headerName, changeName] = useState('Enter Your Name')
+    const [Occupation, changeOccupation] = useState('Enter Your Occupation')
+    const [email, changeEmail] = useState('Provide an Email Address')
+
+
+    const changeHeaderName = (e) => {
+
+        changeName(e.target.value)
+        
+    }
+
+    const changeOccupationName = (e) => {
+
+        changeOccupation(e.target.value)
+    }
+
+    const changeEmailName = (e) => {
+
+
+        changeEmail(e.target.value)
+    }
+
 
     return (
 
         <>
-                <Sidebar />
+            <div className="App">
+                <div className="sidebar">
+
+                    <h1>Resume Creator</h1>
+
+                    <label htmlFor="Name">Enter Your Name</label>
+                    <input type="text" onChange={changeHeaderName} placeholder='John Doe'/>
+
+                    <label htmlFor="Occupation">Enter Your Occupation</label>
+                    <input type="text" onChange={changeOccupationName} placeholder='Financial Advisor' id='Occupation'/>
+
+
+                    <label htmlFor="Name">Enter Your Email-Address</label>
+                    <input type="text" onChange={changeEmailName} placeholder='react@icloud.com'/>
+
+                    <label htmlFor="Name">Enter Your phoneNumber</label>
+                    <input type="text" onChange={changeOccupationName} placeholder='Financial Advisor'/>
+
+                </div>
+
+                <div className="cvDisplay">
+
+                    <div className="header">
+
+                            <img src="" alt="" id='images'/>
+
+                            <div className="headerContents">
+                                <div>{headerName}</div>
+                                <div>{Occupation}</div>
+                            </div>
+                            
+                    </div>
+
+                    <div className="cvBody">
+                        
+                            <div id="sidePanel">
+
+                                <h3>Contact Information</h3>
+
+                                <div>{email}</div>
+
+
+                            </div>
+
+                            <div id="cvInfo">
+
+                            </div>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
--- cvApplication/src/main.jsx
+++ cvApplication/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/css/style.css'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <App />
