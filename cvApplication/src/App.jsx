0bec4fa44ade906bc78026573b7cebbfedacdbe4
Elijah <elijahmoye96@gmail.com>
2025-01-19 21:28:58 -0500

    Website looks good
    Gonna change the branch to try a differnt stratefy
    and see where we can go from there but I love the
    way it looks and I can see koda just chilling by
    my side while I code and shelby is making dinner.
    Crazy just one week ago I was coding in the dark
    high in a dirty room watching berleezy yeah I miss
    it too

--- cvApplication/src/App.jsx
+++ cvApplication/src/App.jsx
@@ -1,4 +1,9 @@
+import { use } from 'react'
 import '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/css/cvDisplay.css'
+import mail from '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/assets/Reference/envelope-solid.png'
+import phone from '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/assets/Reference/phone-solid.png'
+import address from '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/assets/Reference/maps-and-flags.png'
+
 import { useState } from 'react'
 
 export function App(){
@@ -6,6 +11,9 @@ export function App(){
     const [headerName, changeName] = useState('Enter Your Name')
     const [Occupation, changeOccupation] = useState('Enter Your Occupation')
     const [email, changeEmail] = useState('Provide an Email Address')
+    const [phoneNumber, changeNumber] = useState('Provide Your Phone Number ')
+    const [Address, changeAdress] = useState('Address Info')
+    
 
 
     const changeHeaderName = (e) => {
@@ -25,6 +33,11 @@ export function App(){
         changeEmail(e.target.value)
     }
 
+    const changePhoneNumber = (e) => {
+
+        changeNumber(e.target.value)
+    }
+
 
     return (
 
@@ -34,19 +47,27 @@ export function App(){
 
                     <h1>Resume Creator</h1>
 
-                    <label htmlFor="Name">Enter Your Name</label>
-                    <input type="text" onChange={changeHeaderName} placeholder='John Doe'/>
+                    <div className="labels">
 
-                    <label htmlFor="Occupation">Enter Your Occupation</label>
-                    <input type="text" onChange={changeOccupationName} placeholder='Financial Advisor' id='Occupation'/>
+                    <div className="labelStyle">
+                        <label htmlFor="Name">Name</label>
+                        <input type="text" onChange={changeHeaderName} placeholder='John Doe'/>
 
+                        <label htmlFor="Name">Email-Address</label>
+                        <input type="text" onChange={changeEmailName} placeholder='react@icloud.com'/>
+                    </div>
 
-                    <label htmlFor="Name">Enter Your Email-Address</label>
-                    <input type="text" onChange={changeEmailName} placeholder='react@icloud.com'/>
+                    <div className="labelStyle">
 
-                    <label htmlFor="Name">Enter Your phoneNumber</label>
-                    <input type="text" onChange={changeOccupationName} placeholder='Financial Advisor'/>
+                        <label htmlFor="Occupation">Occupation</label>
+                        <input type="text" onChange={changeOccupationName} placeholder='Financial Advisor' id='Occupation'/>
 
+                        <label htmlFor="Name">Phone Number</label>
+                        <input type="text" onChange={changePhoneNumber} placeholder='XXX-XXX-XXXX'/>
+
+                    </div>
+
+                </div>
                 </div>
 
                 <div className="cvDisplay">
@@ -56,7 +77,7 @@ export function App(){
                             <img src="" alt="" id='images'/>
 
                             <div className="headerContents">
-                                <div>{headerName}</div>
+                                <h3>{headerName}</h3>
                                 <div>{Occupation}</div>
                             </div>
                             
@@ -67,8 +88,25 @@ export function App(){
                             <div id="sidePanel">
 
                                 <h3>Contact Information</h3>
+                                
+                                <div id="contactDetails">
 
-                                <div>{email}</div>
+                                    <div className="contactInfo">
+                                        <img src={mail} alt="" width='30px' height='30px' id='icon'/>
+                                        <div>{email}</div>
+                                    </div>
+
+                                    <div className="contactInfo">
+                                        <img src={phone} alt="" width='30px' height='30px' id='icon' />
+                                        <div>{phoneNumber}</div>
+                                    </div>
+
+                                    <div className="contactInfo">
+
+                                        <img src={address} alt="" width='30px' height='30px' id='icon' />
+                                        <div>{Address}</div>
+                                    </div>
+                                </div>
 
 
                             </div>
--- cvApplication/src/jsx/Appcomponents.jsx
+++ cvApplication/src/jsx/Appcomponents.jsx
@@ -1,2 +1,19 @@
 import { useState } from "react"
 
+export function Test({names}){
+
+    const [names, changeMe] = useState(names)
+
+    changeMeNow = () => {
+
+        changeMe(e.target.value)
+    } 
+
+    return (
+
+        <>
+            <div>{names}</div>
+        </>
+    )
+
+}
