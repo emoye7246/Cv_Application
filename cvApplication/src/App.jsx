55a86e7397388f9dd96f54ec7a3f5d7a3e00eb4b
Elijah <elijahmoye96@gmail.com>
2025-01-21 15:02:16 -0500

    Gonna try something
    So the education portion of the project is
    completed now what im going to try and do
    is see if there would be any way for me to break
    down this code because its gotta be anither way

--- cvApplication/src/App.jsx
+++ cvApplication/src/App.jsx
@@ -3,7 +3,6 @@ import '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src
 import mail from '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/assets/Reference/envelope-solid.png'
 import phone from '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/assets/Reference/phone-solid.png'
 import address from '/Users/elijahmoye/Desktop/cvApplication/Cv_Application/cvApplication/src/assets/Reference/maps-and-flags.png'
-
 import { useState } from 'react'
 
 export function App(){
@@ -13,6 +12,7 @@ export function App(){
     const [email, changeEmail] = useState('Provide an Email Address')
     const [phoneNumber, changeNumber] = useState('Provide Your Phone Number ')
     const [Address, changeAdress] = useState('Address Info')
+    const [Education, changeEducation] = useState('Education')
     
 
 
@@ -38,43 +38,81 @@ export function App(){
         changeNumber(e.target.value)
     }
 
+    const changeEducationName = (e) => {
+
+        changeEducation(e.target.value)
+    }
+
 
     return (
 
         <>
             <div className="App">
+
                 <div className="sidebar">
 
                     <h1>Resume Creator</h1>
 
+                    <h3>Information</h3>
+
                     <div className="labels">
 
-                    <div className="labelStyle">
-                        <label htmlFor="Name">Name</label>
-                        <input type="text" onChange={changeHeaderName} placeholder='John Doe'/>
+                        <div className="labelStyle">
+                            <label htmlFor="Name">Name</label>
+                            <input type="text" onChange={changeHeaderName} placeholder='John Doe'/>
 
-                        <label htmlFor="Name">Email-Address</label>
-                        <input type="text" onChange={changeEmailName} placeholder='react@icloud.com'/>
+                            <label htmlFor="Name">Email-Address</label>
+                            <input type="text" onChange={changeEmailName} placeholder='react@icloud.com'/>
+                        </div>
+
+                        <div className="labelStyle">
+
+                            <label htmlFor="Occupation">Occupation</label>
+                            <input type="text" onChange={changeOccupationName} placeholder='Financial Advisor' id='Occupation'/>
+
+                            <label htmlFor="Name">Phone Number</label>
+                            <input type="text" onChange={changePhoneNumber} placeholder='XXX-XXX-XXXX'/>
+
+                        </div>
+
                     </div>
 
-                    <div className="labelStyle">
+                    <h3>Education</h3>
 
-                        <label htmlFor="Occupation">Occupation</label>
-                        <input type="text" onChange={changeOccupationName} placeholder='Financial Advisor' id='Occupation'/>
+                        <div className="educationInfo">
 
-                        <label htmlFor="Name">Phone Number</label>
-                        <input type="text" onChange={changePhoneNumber} placeholder='XXX-XXX-XXXX'/>
+                                <div className="eduLabel">
 
-                    </div>
+                                    <label htmlFor="Education">Education</label>
+                                    <input type="text" onChange={changeEducationName}/>
 
-                </div>
+                                    <label htmlFor="">Field of Study</label>
+                                    <input type="text"  />
+                                </div>
+
+                                <div className="eduLabel">
+
+                                    <label htmlFor="Start">Start Term</label>
+                                    <input type="number" max={2025}/>
+
+                                    <label htmlFor="Start">End Term</label>
+                                    <input type="number" max={2025}/>
+                                </div>
+
+                            
+                        </div>
+
+
+
+                        
+                
                 </div>
 
                 <div className="cvDisplay">
 
                     <div className="header">
 
-                            <img src="" alt="" id='images'/>
+                            <img src='' height='30px' width='30px' alt="" id='images'/>
 
                             <div className="headerContents">
                                 <h3>{headerName}</h3>
@@ -106,6 +144,18 @@ export function App(){
                                         <img src={address} alt="" width='30px' height='30px' id='icon' />
                                         <div>{Address}</div>
                                     </div>
+                                </div>
+
+                                <div id="educationDisplay">
+
+                                    <h3>Education</h3>
+
+                                    <div className="educationDisplay">
+
+                                        <img src="" alt="" id='Icon' />
+                                        <div>{Education}</div>
+                                    </div>
+
                                 </div>
 
 
