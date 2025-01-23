d48148e35fb88d8241aaaa50620bc12827569dce
Elijah <elijahmoye96@gmail.com>
2025-01-22 19:35:55 -0500

    Ugly but better
    Im getting used to all of the functinality
    of this react thing but its kind of confusing
    and all over the place Im gonna try and clean
    it up when I get the chance Im a mess right now

---
+++ cvApplication/src/jsx/Appcomponents.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+     
+    export const Names = (prop) => {
+
+     return(   
+        <>
+
+            <h3>{prop.Name}</h3>
+            
+        </>
+     )
+
+    }
+
+   export  const Occupation = (prop) => {
+
+        return(   
+            <>
+                <div>{prop.Occupation}</div>
+            </>
+         )
+
+    }
+
+    
+
+
+
